51a52aa9c1dd5df36e659384fcf36398749634a6
Mr Meeseeks <14327806+iammrmeeseeks@users.noreply.github.com>
2024-06-08 10:22:01 +0530

    array example

---
+++ goTopics/goTopics.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Go Topics")
+	fmt.Println("example of arrays and slices")
+
+	var arr [3]int = [3]int{1, 2, 3}
+
+	fmt.Println("array arr: ", arr)
+}
